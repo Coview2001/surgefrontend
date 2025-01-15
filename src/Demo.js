a4b6f6e65002309cedd0958c06e167490d28d38a
tpsdevlop <developers@thoughtprocess.co.in>
2025-01-15 15:43:44 +0530

    13-01-25 2

---
+++ src/Demo.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+function Demo() {
+    const [courses, setCourses] = useState([]);
+    useEffect(() => {
+        fetchData();
+    }, []);
+    
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://surgebackend.azurewebsites.net//');
+        const data = response.data;
+
+        setCourses(data.Message);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+  return (
+    <div>
+        {courses}
+      <p>From Varu 1</p>123
+    </div>
+  )
+}
+
+export default Demo
