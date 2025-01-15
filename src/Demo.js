789dfa2800a9dbeacf124464f27d14949eb8a2e5
tpsdevlop <developers@thoughtprocess.co.in>
2025-01-15 16:34:10 +0530

    15-01-25 2

--- src/Demo.js
+++ src/Demo.js
@@ -19,9 +19,9 @@ function Demo() {
       }
     };
   return (
-    <div>
-        {courses}
-      <p>From Varu 1</p>123
+    <div><div className='text-center mt-5 fw-bold border border-success p-2 rounded '  style={{color:"green",fontSize:"16px"}} > {courses}</div> 
+        
+      <p className='text-end fw-italic pe-5' style={{fontSize:"14px"}}>- From Varu </p>
     </div>
   )
 }
